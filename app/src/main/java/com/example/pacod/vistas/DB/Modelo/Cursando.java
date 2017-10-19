45a780f957fa09e836ed55cd6204bc565da127a9
consvic <cocoy_shuffle@me.com>
2017-10-18 23:52:57 -0500

    Cursando.java
    FKs añadidas con getters

--- app/src/main/java/com/example/pacod/vistas/DB/Modelo/Cursando.java
+++ app/src/main/java/com/example/pacod/vistas/DB/Modelo/Cursando.java
@@ -6,18 +6,56 @@ package com.example.pacod.vistas.DB.Modelo;
 
 public class Cursando {
 
+    private int id_cursando;
+    private int id_usuario;
+    private int id_materia;
+    private String nombre;
+    private String descripcion;
+    private String horario;
+    private String hora_entrada;
+    private String hora_salida;
 
-    public String nombre;
-    public String descripcion;
-    public String horario;
-    public String hora_entrada;
-    public String hora_salida;
 
-    public Cursando(String nombre, String descripcion, String horario, String hora_entrada, String hora_salida) {
+    public Cursando(int id_cursando, int id_usuario, int id_materia, String nombre, String descripcion, String horario, String hora_entrada, String hora_salida) {
+        this.id_cursando = id_cursando;
+        this.id_usuario = id_usuario;
+        this.id_materia = id_materia;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.horario = horario;
         this.hora_entrada = hora_entrada;
         this.hora_salida = hora_salida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public String getHora_entrada() {
+        return hora_entrada;
+    }
+
+    public String getHora_salida() {
+        return hora_salida;
+    }
+
+    public int getId_cursando() {
+        return id_cursando;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public int getId_materia() {
+        return id_materia;
     }
 }
